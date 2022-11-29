b6786c062297d2e7cef1c5bfedb04140648da34f
Nikolay Gladkovskiy <NikkoGladkko@users.noreply.github.com>
2022-11-29 20:43:25 +0300

    first commit

---
+++ GladkovskiyHomework/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  GladkovskiyHomework
+//
+//  Created by Teacher on 29.11.2022.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad(){
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 48, weight: .thin)
+        label.textColor = .white
+        label.numberOfLines = 0
+        label.text = "Николай Гладковский"
+        
+        view.addSubview(label)
+        
+        label.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().offset(-32.0)
+            make.leading.equalToSuperview().offset(32.0)
+        }
+        
+        view.backgroundColor = .orange
+    }
+}
