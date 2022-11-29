036d2a208639c1600a8204fdb09a033baba1199d
Nikolay Gladkovskiy <NikkoGladkko@users.noreply.github.com>
2022-11-29 20:26:02 +0300

    Initial Commit

---
+++ GladkovskiyHomework/GladkovskiyHomework.h
@@ -0,0 +1,18 @@
+//
+//  GladkovskiyHomework.h
+//  GladkovskiyHomework
+//
+//  Created by Teacher on 29.11.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GladkovskiyHomework.
+FOUNDATION_EXPORT double GladkovskiyHomeworkVersionNumber;
+
+//! Project version string for GladkovskiyHomework.
+FOUNDATION_EXPORT const unsigned char GladkovskiyHomeworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GladkovskiyHomework/PublicHeader.h>
+
+
