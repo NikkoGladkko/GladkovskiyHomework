af600f4ce79990c3fc8517a94187a03a532d3370
Nikolay Gladkovskiy <NikkoGladkko@users.noreply.github.com>
2022-12-01 01:16:35 +0300

    add asset

--- GladkovskiyHomework/HomeViewController.swift
+++ GladkovskiyHomework/HomeViewController.swift
@@ -13,15 +13,27 @@ import OtusHomework
 public class HomeViewController: UIViewController {
     public override func viewDidLoad(){
         let label = UILabel()
-        label.font = .systemFont(ofSize: 48, weight: .thin)
+        label.font = .systemFont(ofSize: 24, weight: .thin)
         label.textColor = .white
         label.numberOfLines = 0
         label.text = "Николай Гладковский"
+        let currentBundle = Bundle(for: HomeViewController.self)
+        let image = UIImage(named: "profileImage", in: currentBundle, with: .none)
+        let profileView = UIImageView(image: image)
+        profileView.clipsToBounds = true
+        profileView.layer.cornerRadius = 64
         
+        view.addSubview(profileView)
         view.addSubview(label)
         
+        profileView.snp.makeConstraints { make in
+            make.bottom.equalTo(label.snp.top).offset(32)
+            make.width.equalTo(128.0)
+            make.height.equalTo(128.0)
+        }
+        
         label.snp.makeConstraints { make in
-            make.centerY.equalToSuperview()
+            make.bottom.equalToSuperview().offset(-32.0)
             make.trailing.equalToSuperview().offset(-32.0)
             make.leading.equalToSuperview().offset(32.0)
         }
