ad420dc644c1d5d7b3447a518353695941d13d49
Nikolay Gladkovskiy <NikkoGladkko@users.noreply.github.com>
2022-12-01 01:22:14 +0300

    fix profile image

--- GladkovskiyHomework/HomeViewController.swift
+++ GladkovskiyHomework/HomeViewController.swift
@@ -13,27 +13,31 @@ import OtusHomework
 public class HomeViewController: UIViewController {
     public override func viewDidLoad(){
         let label = UILabel()
-        label.font = .systemFont(ofSize: 24, weight: .thin)
+        label.font = .systemFont(ofSize: 32, weight: .thin)
         label.textColor = .white
         label.numberOfLines = 0
+        label.textAlignment = .center
         label.text = "Николай Гладковский"
+        label.layer.borderWidth = 8
+        label.layer.borderColor = UIColor.white.cgColor
+        
         let currentBundle = Bundle(for: HomeViewController.self)
         let image = UIImage(named: "profileImage", in: currentBundle, with: .none)
         let profileView = UIImageView(image: image)
         profileView.clipsToBounds = true
-        profileView.layer.cornerRadius = 64
+        profileView.layer.cornerRadius = 80
         
         view.addSubview(profileView)
         view.addSubview(label)
         
         profileView.snp.makeConstraints { make in
-            make.bottom.equalTo(label.snp.top).offset(32)
-            make.width.equalTo(128.0)
-            make.height.equalTo(128.0)
+            make.bottom.equalTo(label.snp.top).offset(-32)
+            make.width.equalTo(160.0)
+            make.height.equalTo(160.0)
+            make.center.equalToSuperview()
         }
         
         label.snp.makeConstraints { make in
-            make.bottom.equalToSuperview().offset(-32.0)
             make.trailing.equalToSuperview().offset(-32.0)
             make.leading.equalToSuperview().offset(32.0)
         }
